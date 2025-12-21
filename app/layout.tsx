a285ae03517f90f74d5d3c751bec011aa0e77535
ndizeyedavid <davidndizeye101@gmail.com>
2025-12-21 14:38:17 +0200

    admin layout fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "react-international-phone/style.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import PublicShell from "./components/PublicShell";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <Header />
-        {children}
-        <Footer />
+        <PublicShell>{children}</PublicShell>
       </body>
     </html>
   );
