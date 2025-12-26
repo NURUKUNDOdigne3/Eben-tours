dd6cfeabccde17790c31aa43be3f8fcb18919ce8
ndizeyedavid <davidndizeye101@gmail.com>
2025-12-26 14:23:07 +0200

    upd: added logo branding to the admin auth forms

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,16 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-[#f6f8f7] px-4 py-10">
-      <div className="mx-auto flex max-w-xl justify-center">
+      <div className="mx-auto flex flex-col items-center gap-5 max-w-xl justify-center">
+        <Image
+          src="/log.webp"
+          width={100}
+          height={100}
+          alt="Eben Safari Tours Logo"
+        />
         <SignIn forceRedirectUrl="/admin" />
       </div>
     </div>
